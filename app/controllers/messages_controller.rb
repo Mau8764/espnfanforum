719aa95753a1622b93a7b9ef5623e75a6efea34d
Mau8764 <Mau8764@github.com>
2022-02-01 21:54:12 -0600

    Add CRUD actions and views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,10 @@
 class MessagesController < ApplicationController
-
+  before_action :find_message, only: [:edit, :update, :show, :destroy]
   def index
     @messages = Message.all.order(:created_at)
+  end
+
+  def show
   end
   
   def new
@@ -9,7 +12,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(messages_params)
+    @message = Message.new(message_params)
 
     if @message.save
       redirect_to root_path
@@ -18,9 +21,30 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @message.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
-  def messages_params
+  def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
