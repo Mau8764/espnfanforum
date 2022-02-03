db0d40cd5cbbf5985bb5d93cb334ca5753416343
Mau8764 <Mau8764@github.com>
2022-02-02 22:19:42 -0600

    Add comments controller, model and views to messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :messages
+  resources :messages do
+    resources :comments
+  end
   root "messages#index"
 end
